57790ee2876b681520debdd88d8688c869bf5083
Abhishek kumar <Abhishek kumar@DESKTOP-S7U7C2M>
2019-04-06 06:12:54 +0530

    commit @ 6

--- wastedPotentials/src/main/java/wastedPotentials/XLSReader.java
+++ wastedPotentials/src/main/java/wastedPotentials/XLSReader.java
@@ -3,6 +3,8 @@ package wastedPotentials;
 import java.io.File;
 import java.util.ArrayList;
 
+import javax.swing.Spring;
+
 import com.codoid.products.exception.FilloException;
 import com.codoid.products.fillo.Connection;
 import com.codoid.products.fillo.Fillo;
@@ -29,8 +31,13 @@ public class XLSReader {
     private ArrayList<ArrayList<String>>  arrProjectLevelDetails;
     private ArrayList<ArrayList<String>> arrProjectSkeletons;
     private ArrayList<ArrayList<String>> arrQueries;
+    private ArrayList<ArrayList<String>> arrtest_paramName_paramValue_csv;
     private ArrayList<String> arrQuery;
-    public XLSReader(String filePath){
+    private String pldquery;
+    private String tcquery;      
+    private String dquery;
+    private String psquery;
+    public XLSReader(String filePath) throws Exception{
        try {
     	this.fillo = new Fillo();
         this.filePath = filePath;
@@ -51,17 +58,19 @@ public class XLSReader {
          //psdquery  tcquery   dquery    psquery
 
          for(int i=0;i<arrQueries.size();i++) {
-        	if(arrQueries.get(0).get(i)=="psdquery") {
+        	if(arrQueries.get(0).get(i)=="pldquery") {
+        		pldquery=arrQueries.get(0).get(1);
             	
             }
         	else if(arrQueries.get(0).get(i)=="tcquery") {
+        		tcquery=arrQueries.get(0).get(1);
             	
             }
         	else if(arrQueries.get(0).get(i)=="dquery") {
-        	
+        	dquery=arrQueries.get(0).get(1);
 }
         	else if(arrQueries.get(0).get(i)=="psquery") {
-	
+	psquery=arrQueries.get(0).get(1);
 }
         	else {
         		throw new Exception("query name is invalid");
@@ -69,15 +78,49 @@ public class XLSReader {
 
         	
         }
-         
-        maven_Needed=recordset1.getField("maven_Needed");
+         Recordset pldRS=connection.executeQuery(pldquery);
+        maven_Needed=pldRS.getField("maven_Needed");
         if(!maven_Needed.isEmpty() && maven_Needed.equals("Y")) {
-        	
+        	maven_project_ModelVersion=pldRS.getField("maven_project_ModelVersion");
+        	maven_project_GroupId=pldRS.getField(maven_project_GroupId);
+        	maven_project_ArtifactId=pldRS.getField("maven_project_ArtifactId");
+        	maven_project_Version=pldRS.getField("maven_project_Version");
+        	Recordset dRS=connection.executeQuery(dquery);
+        	while(dRS.next()) {
+        	ArrayList<String> singledependency= new ArrayList<String>();
+        	singledependency.add(dRS.getField("dependencyArtifactId"));
+        	singledependency.add(dRS.getField("dependencyGroupId"));
+        	singledependency.add(dRS.getField("dependencyVersion"));
+            arrDependenciesMaven.add(singledependency);
+        	}
         }
-        testNg_Needed=recordset1.getField("testNg_Needed");
+        testNg_Needed=pldRS.getField("testNg_Needed");
         if(!testNg_Needed.isEmpty() && testNg_Needed.equals("Y")) {
+        	String testNg_listenerClasses_csv=pldRS.getField("testNg_listenerClasses_csv");
+        	String testNG_suite_level_paramName_paramValue_csv=pldRS.getField("testNG_suite_level_paramName_paramValue_csv");
+        	String[] listenerstring= testNg_listenerClasses_csv.split(",");
+        	String[] suitelevelparams=testNG_suite_level_paramName_paramValue_csv.split(",");
         	
+        	for(String s:listenerstring) {
+        		arrTestNGListeners.add(s);
+        	}
+    		ArrayList<String> x= new ArrayList<>();
+
+        	for(String s1: suitelevelparams) {
+        		String[] s2=s1.split(":");
+        		for(String s3:s2) {
+        			x.add(s3);
+        		}
+        		arrTestNGSuiteLevelParameters.add(x);
+        		x=new ArrayList<>();
+        		
+        	}
         }
+        this.projectName=pldRS.getField("projectName");
+        this.projectDescription=pldRS.getField("projectDescription");
+        this.projectSkeleton=pldRS.getField("projectSkeleton");
+        this.projectLocation=pldRS.getField("projectLocation");
+
         
     } catch (FilloException e) {
         e.printStackTrace();
